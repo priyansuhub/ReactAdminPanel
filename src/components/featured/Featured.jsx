6a942d07275d0cff3f5ec18e9807d0e858bb974d
Priyansu Hub <muniapriyansu@gmail.com>
2022-05-04 22:04:53 +0530

    Fourth Commit

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,7 +1,40 @@
+import {
+  KeyboardArrowDown,
+  KeyboardArrowUp,
+  MoreVert,
+} from "@mui/icons-material";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
 import React from "react";
 import "./featured.scss";
 const Featured = () => {
-  return <div className="featured">Featured</div>;
+  return (
+    <div className="featured">
+      <div className="top">
+        <h1 className="title">Total Revenue</h1>
+        <MoreVert />
+      </div>
+      <div className="bottom">
+        <div className="featuredChart">
+          <CircularProgressbar value={70} text={"70%"} strokeWidth={4} />
+        </div>
+        <p className="title">Total sales made today</p>
+        <p className="amount">$420</p>
+        <p className="desc">Previous Transactions processing</p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult">
+              <KeyboardArrowDown />
+              <KeyboardArrowUp />
+              <div className="resultAmount">$12k</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
